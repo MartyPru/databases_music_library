6714f45b5e007df0c636e9e81ffa7bf2ab6405d4
Martynas Prusevicius <mprusevicius@gmail.com>
2023-11-28 12:38:44 +0000

    Complete challenge 7

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -16,3 +16,10 @@ class AlbumRepository():
         matching_result = self.connection.execute('SELECT * FROM albums WHERE id = %s;', [artist_id])[0]
         matching_object = Album(matching_result['id'], matching_result['title'], matching_result['release_year'], matching_result['artist_id'])
         return matching_object
+    
+    def create(self, album):
+        self.connection.execute("INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)",
+                                [album.title, album.release_year, album.artist_id])
+        
+    def delete(self, album_id):
+        self.connection.execute("DELETE FROM albums WHERE id = %s", [album_id])
--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -32,3 +32,55 @@ def test_find_returns_matching_element(db_connection):
     repo = AlbumRepository(db_connection)
     matching_album = repo.find(1)
     assert matching_album == Album(1, 'Doolittle', 1989, 1)
+
+
+"""
+When calling #create with an Album object on instance of repository
+and then calling #all on instance of repository
+Album object should be in result set
+"""
+def test_create(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repo = AlbumRepository(db_connection)
+    new_album = Album(None, 'AlbumTest', 2023, 2)
+    repo.create(new_album)
+    albums = repo.all()
+    assert albums == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2, 'Surfer Rosa', 1988, 1),
+        Album(3, 'Waterloo', 1974, 2),
+        Album(4, 'Super Trouper', 1980, 2),
+        Album(5, 'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7, 'Folklore', 2020, 3),
+        Album(8, 'I Put a Spell on You', 1965, 4),
+        Album(9, 'Baltimore', 1978, 4),
+        Album(10, 'Here Comes the Sun', 1971, 4),
+        Album(11, 'Fodder on My Wings', 1982, 4),
+        Album(12, 'Ring Ring', 1973, 2),
+        Album(13, 'AlbumTest', 2023, 2)
+    ]
+
+"""
+When calling #delete on instance of repository with an ID
+And then calling #all on instance of repository
+Deleted object should not be in result set
+"""
+def test_delete(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repo = AlbumRepository(db_connection)
+    repo.delete(4)
+    albums = repo.all()
+    assert albums == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2, 'Surfer Rosa', 1988, 1),
+        Album(3, 'Waterloo', 1974, 2),
+        Album(5, 'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7, 'Folklore', 2020, 3),
+        Album(8, 'I Put a Spell on You', 1965, 4),
+        Album(9, 'Baltimore', 1978, 4),
+        Album(10, 'Here Comes the Sun', 1971, 4),
+        Album(11, 'Fodder on My Wings', 1982, 4),
+        Album(12, 'Ring Ring', 1973, 2)
+    ]
